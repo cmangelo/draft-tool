216710653c54448718fee73785ff28afd4eead47
cmangelo <christianangelo112@msn.com>
2019-07-26 13:06:45 -0700

    Search functionality working, needs to be styled

--- src/app/shared/pipes/search.pipe.ts
+++ src/app/shared/pipes/search.pipe.ts
@@ -1,15 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { PlayerModel } from 'src/app/+state/entities/player/player.model';
+import { TierModel } from 'src/app/+state/entities/tier/tier.model';
 
 @Pipe({
   name: 'search'
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value: any, term: string): any {
+  transform(value: Array<TierModel>, term: string): any {
     if (!term) return value;
 
-    return value.filter((player: PlayerModel) => (new RegExp(term, 'gi').test(player.name)));
+    return value.map(tier => { return { ...tier, players: tier.players.filter((player: PlayerModel) => (new RegExp(term, 'gi').test(player.name))) } })
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SearchPipe } from './pipes/search.pipe';
+import { FilteredLengthPipe } from './pipes/filtered-length.pipe';
 
 
 
 @NgModule({
-  declarations: [SearchPipe],
+  declarations: [SearchPipe, FilteredLengthPipe],
   imports: [
     CommonModule
   ],
-  exports: [SearchPipe]
+  exports: [SearchPipe, FilteredLengthPipe]
 })
 export class SharedModule { }
